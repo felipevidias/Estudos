4169e1aa92a95f286f86078faae0c75a9e72ad51
Felipe <Felipe Dias>
2024-01-10 16:31:50 -0300

    entrada e saida estudos

---
+++ saidaentrada.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        int x,y; 
+        double z;
+        x = 10;
+        y = 20;
+        z = 4.52;
+        System.out.println(String.format("%.2f", z));
+        System.out.println(x);
+        System.out.println(y);
+        System.out.print("Bom dia");
+        System.out.print("Boa noite");
+        System.out.println("");
+        System.out.println("Bom dia");
+        System.out.println("Boa noite");
+    
+    }
+}
