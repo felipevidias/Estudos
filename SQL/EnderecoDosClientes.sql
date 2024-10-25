070324a0dc1197f2ed388877724b090b846ba991
Felipe Vilhena Dias <121449886+felipevidias@users.noreply.github.com>
2024-10-24 22:31:12 -0300

    Add files via upload

---
+++ SQL/EnderecoDosClientes.sql
@@ -0,0 +1,1 @@
+SELECT name,street FROM customers WHERE TRIM(city)='Porto Alegre';
---
+++ SQL/SelectBasico.sql
@@ -0,0 +1,6 @@
+/*SELECT name - Mostra o nome
+FROM customers - da tabela customers
+WHERE state='RS' - onde o estado é equivalente a RS
+*/
+
+SELECT name FROM customers WHERE state='RS';
